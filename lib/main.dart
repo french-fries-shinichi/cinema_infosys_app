e9a936e6f9b1cb7b85e5406c4d8f5a82b925851a
french-fries-shinichi <rainbowfries1@gmail.com>
2026-08-11 05:50:45 +0300

    Added more boilerplate code. And added a background to homepage

--- lib/main.dart
+++ lib/main.dart
@@ -17,18 +17,49 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
+  final String title;
+
+  @override Widget build(BuildContext context) {
+    return const DecoratedBox(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: <Color>[
+            Color(0xFF251B1B),
+            Color(0xFF470707),
+            Color(0xFF000000)
+          ],
+          stops: <double>[0.5],
+          tileMode: TileMode.clamp,
+        ),
+      ),
+    );
+  }
 }
 
-// this is for bakas like me ///////////////////////////
+// this is standard values for bakas like me ///////////////////////////
+//    Gradient:
 // 0%   251B1B
 // 50%  470707
 // 100% 000000
+//
+//    Screen Resolution:
+// W 375 | H 812
 
-class NavigationBar extends StatelessWidget {}
-class ChatbotPage extends StatelessWidget {}
-class ViewPostPage extends StatelessWidget {}
-class NewPostPage extends StatelessWidget {}
-class UserPage extends StatelessWidget {}
+// class NavigationBar extends StatelessWidget {
+//   const NavigationBar({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return x;
+//   }
+// }
+// class ChatbotPage extends StatelessWidget {}
+// class ViewPostPage extends StatelessWidget {}
+// class NewPostPage extends StatelessWidget {}
+// class UserPage extends StatelessWidget {}
